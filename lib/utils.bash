a49df1f4ed124438804892d27ac3bb002f7e501d
Kenneth V. Domingo <hello@kvd.studio>
2025-12-06 15:19:22 +0800

    fix: filter GitHub tags to only include semvers

--- lib/utils.bash
+++ lib/utils.bash
@@ -33,7 +33,7 @@ list_github_tags() {
 list_all_versions() {
 	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
 	# Change this function if infisical has other means of determining installable versions.
-	list_github_tags
+	list_github_tags | grep -oP '^(\d+\.){2}\d+$'
 }
 
 download_release() {
